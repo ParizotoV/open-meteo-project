1de63ad6e5a1e539d68105be8d0ea1c243f597a2
Vinicius Vieira Parizoto <se16095@stellantis.com>
2024-02-07 17:14:54 -0300

    format

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,38 @@
-"use client";
-import React from "react";
-import { Bolt, ArrowLeft } from "lucide-react";
+'use client'
+import React from 'react'
+import { Bolt, ArrowLeft } from 'lucide-react'
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 type HeaderProps = {
-  buttonBack?: boolean;
-};
+  buttonBack?: boolean
+}
 
 const Header: React.FC<HeaderProps> = ({ buttonBack = false }) => {
-  const { push } = useRouter();
+  const { push } = useRouter()
   return (
-    <header className="flex justify-between w-full items-center">
+    <header className='flex justify-between w-full items-center'>
+      
       <div
-        className="bg-[#292248] rounded rounded-full w-[50px] h-[50px] flex items-center justify-center"
-        onClick={() => buttonBack && push("/forecasts")}
+        className='bg-[#292248] rounded rounded-full w-[50px] h-[50px] flex items-center justify-center'
+        onClick={() => buttonBack && push('/forecasts')}
       >
         {buttonBack ? (
-          <ArrowLeft data-testid="back-svg" color="#b3a8ed" />
+          <ArrowLeft data-testid='back-svg' color='#b3a8ed' />
         ) : (
-          <Bolt data-testid="config-svg" color="#b3a8ed" />
+          <Bolt data-testid='config-svg' color='#b3a8ed' />
         )}
       </div>
-      <h2 className="text-white text-2xl font-bold h-auto">ForecastWeather</h2>
+      <h2 className='text-white text-2xl font-bold h-auto'>ForecastWeather</h2>
       <div
-        data-testid={"button-weather"}
-        className="bg-[#292248] rounded rounded-full w-[50px] h-[50px] flex items-center justify-center"
+        data-testid={'button-weather'}
+        className='bg-[#292248] rounded rounded-full w-[50px] h-[50px] flex items-center justify-center'
       >
-        <Image src="/img/weather.svg" alt="weather" width={40} height={40} />
+        <Image src='/img/weather.svg' alt='weather' width={40} height={40} />
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
